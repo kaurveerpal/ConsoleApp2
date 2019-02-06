c489099f60f720079e7156deda599fbcf6642d57
visualstudio <visualstudio@122-PC10>
2019-02-06 10:16:59 -0500

    peanut

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,8 +8,29 @@ namespace ConsoleApp2
 {
     class Program
     {
+        static int i = 0;
         static void Main(string[] args)
         {
+            MethodA();
         }
+        static void MethodA()
+        {
+            while (Peanut())
+            {
+                if(i>10)
+                {
+                    return;
+                }
+            }
+        }
+        static bool Peanut()
+        {
+            i++;
+            return true;
+            Console.ReadLine();
+        }
+       
     }
+         
+    
 }
